368b64912d5931568c3d6034f1ae73098b237a2f
Sriyansh Srivastava <sriyanshsrivastava99@gmail.com>
2026-01-30 14:28:29 +0530

    add templates and render templates

--- application.py
+++ application.py
@@ -1,17 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 # flask application and Configuration
 app = Flask(__name__)
+
 
 # Routes
 @app.route("/")
 @app.route("/home")
 def index():
-    return "<h1>Home Page</h1>"
+    return render_template("home.html")
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template("about.html")
 
 
 # Code Entrypoint
