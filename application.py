d329cbe190d44b07f38c814e87e9f6f477514c92
Sriyansh Srivastava <sriyanshsrivastava99@gmail.com>
2026-01-30 14:49:18 +0530

    take argument/data and render to templates

--- application.py
+++ application.py
@@ -3,16 +3,31 @@ from flask import Flask, render_template
 # flask application and Configuration
 app = Flask(__name__)
 
-
+# Dummy data
+    #posts
+posts=[
+    {
+        "author":"Satish",
+        "title":"Blog post 1",
+        "content": "first post content",
+        "date_posted":"January 30, 2026"
+    },
+{
+        "author":"Ritesh",
+        "title":"Blog post 2",
+        "content": "Post content from ritesh",
+        "date_posted":"January 31, 2026"
+    }
+]
 # Routes
 @app.route("/")
 @app.route("/home")
 def index():
-    return render_template("home.html")
+    return render_template("home.html", posts=posts)
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html", title="About")
 
 
 # Code Entrypoint
