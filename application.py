437bda4b854d9887a3c97bc6e8e2c766d761a7ca
Sriyansh Srivastava <sriyanshsrivastava99@gmail.com>
2026-01-30 14:14:03 +0530

    add /, /home and /about route to the application

--- application.py
+++ application.py
@@ -5,8 +5,13 @@ app = Flask(__name__)
 
 # Routes
 @app.route("/")
+@app.route("/home")
 def index():
-    return "Hello world!"
+    return "<h1>Home Page</h1>"
+
+@app.route("/about")
+def about():
+    return "<h1>About Page</h1>"
 
 
 # Code Entrypoint
